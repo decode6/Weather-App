123392b24abe13109e94c9fd723ffa4911ce0632
Decode <amitkumar696945@gmail.com>
2024-01-07 18:09:17 +0530

    initialized

---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const WeatherApp = () => {
+  return (
+    <> 
+    <div className='container'>
+     <div className="weather">
+        <div className="search">
+            <input type="text" placeholder='Enter City Name' />
+        </div>
+     </div>
+    </div>
+    </>
+   
+  )
+}
+
+export default WeatherApp
