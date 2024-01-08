46f20afda635b11b90bdfa97a9f073c8f108cb03
Decode <amitkumar696945@gmail.com>
2024-01-08 23:49:50 +0530

    api

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -15,23 +15,29 @@ const WeatherApp = () => {
   })
   const [name,setName] = useState("");
   useEffect (() => {
-   const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=c562e8eb7ba07cb708d2d51aca38375a&units=metric";
+   
+  }, [])
+
+  const handleClick = () =>{
+    if(name!==""){
+      const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=c562e8eb7ba07cb708d2d51aca38375a&units=metric";
     axios.get(apiUrl)
     .then(res => {
       setData({...data, celcius: res.data.main.temp, name: res.data.name, 
         humidity: res.data.main.humidity, speed: res.data.wind.speed})
     })
     .catch(err => console.error(err));
-  }, [])
+    }
+  }
 
   return (
     <>
       <div className="container">
         <div className="weather">
           <div className="search">
-            <input type="text" placeholder="Enter City Name" />
+            <input type="text" placeholder="Enter City Name"  onChange={e =>setName(e.target.value)}/>
             <button>
-              <img src={search_icon} alt="" />
+              <img src={search_icon} onClick={handleClick} alt="" />
             </button>
           </div>
           <div className="weather-info">
